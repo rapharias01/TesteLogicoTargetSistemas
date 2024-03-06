c13d9be7f87d2d226be2d73e1260a893a7bf4a81
Raphael Farias <raphaddf01@gmail.com>
2024-03-06 15:03:29 -0300

    Questao 2 pronta

--- TesteLogicoQuestao2/Program.cs
+++ TesteLogicoQuestao2/Program.cs
@@ -10,6 +10,28 @@ namespace TesteLogicoQuestao2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Informe um número:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+
+            List<int> fibonacci = new List<int> { 0, 1 };
+
+
+            while (fibonacci[fibonacci.Count - 1] < numero)
+            {
+                int proximoNumero = fibonacci[fibonacci.Count - 1] + fibonacci[fibonacci.Count - 2];
+                fibonacci.Add(proximoNumero);
+            }
+
+            if (fibonacci.Contains(numero))
+            {
+                Console.WriteLine($"O número {numero} pertence à sequência de Fibonacci.");
+            }
+            else
+            {
+                Console.WriteLine($"O número {numero} não pertence à sequência de Fibonacci.");
+            }
+            Console.ReadLine();//sem utilidade esta aqui apenas para teste
         }
     }
 }
