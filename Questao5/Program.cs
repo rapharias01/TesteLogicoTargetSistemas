31d7993fb02e584b1b1d08c3f6edff562a1b248d
Raphael Farias <raphaddf01@gmail.com>
2024-03-06 15:28:55 -0300

    Questao 5

---
+++ Questao5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Questao5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringPadrao = "TargetSistemas";
+            char[] vetorChar = stringPadrao.ToCharArray();
+            int esquerda = 0;
+            int direita = stringPadrao.Length - 1;
+
+            while (esquerda < direita)
+            {                
+                char pos = vetorChar[esquerda];
+                vetorChar[esquerda] = vetorChar[direita];
+                vetorChar[direita] = pos;
+
+                esquerda++;
+                direita--;
+            }
+
+            string reversedString = new string(vetorChar);
+            Console.WriteLine(reversedString);
+            Console.ReadLine();//sem utilidade esta aqui apenas para teste
+        }
+    }
+}
